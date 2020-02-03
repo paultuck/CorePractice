09bfdcc80b45ac1923deba7de066330e5f1a45ed
paultuck <paultuck79@gmail.com>
2020-02-03 06:26:06 +0000

    Business Logic and Repo Layers
    Setting up where I am going to locate the business logic and model repositories in the solution

---
+++ CorePractice.Tests/Controllers/UserBusinessLogicTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CorePractice.Tests.Controllers
+{
+    [TestClass]
+    public class UserBusinessLogicTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

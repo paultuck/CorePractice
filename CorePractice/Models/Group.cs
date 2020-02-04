dcab601884843ccec8c491273404e1f26e823d0e
paultuck <paultuck79@gmail.com>
2020-02-04 16:20:10 +1100

    Fixing the EF model to have a many to many relationship between Students and Groups

--- CorePractice/Models/Group.cs
+++ CorePractice/Models/Group.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace CorePractice.Models
 {
     public class Group
     {
+        [Required]
         public int GroupId { get; set; }
+
+        [MaxLength(50)]
+        [Required]
         public string GroupName { get; set; }
+
+        [MaxLength(256)]
         public string Description { get; set; }
+
+        public List<User> Users { get; set; }
     }
 }
--- CorePractice/Models/User.cs
+++ CorePractice/Models/User.cs
@@ -2,20 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace CorePractice.Models
 {
     public class User
     {
+        [Required]
         public int UserId { get; set; }
+
+        [MaxLength(50)]
+        [Required]
         public string Username { get; set; }
+
+        [MaxLength(128)]
+        [Required]
         public string Password { get; set; }
+
+        [MaxLength(100)]
+        [Required]
         public string Firstname { get; set; }
+
+        [MaxLength(100)]
+        [Required]
         public string Lastname { get; set; }
+
         public DateTime DateOfBirth { get; set; }
+
+        [MaxLength(256)]
+        [Required]
         public string Email { get; set; }
+
+        [MaxLength(50)]
         public string Phone { get; set; }
+
+        [MaxLength(50)]
         public string Mobile { get; set; }
+
         public List<Group> Groups { get; set; }
 
         public User(int UserId, 
